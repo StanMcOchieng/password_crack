0d9275e4cb75a97e61fc6d37f8720b97f43f0182
StanMcOchieng <mcochiengstan@gmail.com>
2023-03-07 09:37:13 +0300

    initial

---
+++ password.py
@@ -0,0 +1,18 @@
+from random import *
+import os
+
+your_pwd = input("Enter a Password:")
+
+pwd =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+pwd = ""
+
+while(pw!= your_pwd):
+    pw=""
+    for letter in range(len(your))
+        guess_pwd_=pwd[randint(0,17)]
+        pw=str(guess_pwd)+str(pw)
+        print(pw)
+        print("Cracking Password...Please wait")
+        os.system("cls")
+
+print("Your password is:",pw)
